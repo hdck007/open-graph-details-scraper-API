64168fbb1b77f7086db7d834dee7f7cd0f9f0272
hari <haridarshanofficial@gmail.com>
2022-12-27 20:23:49 +0530

    fix(api): return a info message when url not provided

--- api/index.js
+++ api/index.js
@@ -8,6 +8,9 @@ async function getOGData(url) {
 
 
 export default async function handler(request, response) {
+  if(!request.query.url) {
+    return response.send("Hey there! Please provide a url to get the OG data, url as query parameter. Simply append '?url=<the url>'");
+  }
   const { result ,error} = await getOGData(request.query.url);
   if (error) {
     return response.status(500).json({ error, isError: true });
