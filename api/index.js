f277e9498dd18d5432322074d9b839be15aa35dc
hari <haridarshanofficial@gmail.com>
2022-12-27 20:27:54 +0530

    feat: made the info message better

--- api/index.js
+++ api/index.js
@@ -1,19 +1,20 @@
 const ogs = require('open-graph-scraper');
 
 async function getOGData(url) {
-  const options = { url };
-  const data = await ogs(options)
-  return data;
+	const options = { url };
+	const data = await ogs(options);
+	return data;
 }
 
-
 export default async function handler(request, response) {
-  if(!request.query.url) {
-    return response.send("Hey there! Please provide a url to get the OG data, url as query parameter. Simply append '?url=<the url>'");
-  }
-  const { result ,error} = await getOGData(request.query.url);
-  if (error) {
-    return response.status(500).json({ error, isError: true });
-  }
-  response.status(200).json(result);  
+	if (!request.query.url) {
+		return response.send(
+			"<h1>Hey there! Please provide a url to get the OG data, url as query parameter.</h1> <p>Simply append <code><pre>'?url=<the url>'</pre></code></p>"
+		);
+	}
+	const { result, error } = await getOGData(request.query.url);
+	if (error) {
+		return response.status(500).json({ error, isError: true });
+	}
+	response.status(200).json(result);
 }
